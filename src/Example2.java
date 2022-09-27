a8641c9d664e3e54d315d4652f7a93cb1c0559c5
joseluis-muriach <alumur497@ieselcaminas.org>
2022-09-27 11:40:53 +0200

    finish

--- src/Example2.java
+++ src/Example2.java
@@ -1,5 +1,6 @@
 public class Example2 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int square = 5 * 5;
+        System.out.println(square);
     }
 }
