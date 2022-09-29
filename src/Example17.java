a4742a9ca0ccbc2c4bcad3119bd7612d1e7df7cb
joseluis-muriach <alumur497@ieselcaminas.org>
2022-09-29 11:05:31 +0200

    Example17 completed

--- src/Example17.java
+++ src/Example17.java
@@ -21,19 +21,62 @@ public class Example17 {
             seconds++;
             if(minutes < 59){
                 if (hours < 24) {
+                    if (seconds < 10) {
+                        if (minutes < 10) {
+                            if (hours < 10) {
+                                System.out.println("0" + hours + ":0" + minutes + ":0" + seconds);
+                            } else {
+                                System.out.println(hours + ":0" + minutes + ":0" + seconds);
+                            }
+                        } else {
+                            System.out.println(hours + ":" + minutes + ":0" + seconds);
+                        }
+                    } else if (minutes < 10){
+                        if (hours < 10) {
+                            System.out.println("0" + hours + ":0" + minutes + ":" + seconds);
+                        } else {
+                            System.out.println(hours + ":0" + minutes + ":" + seconds);
+                        }
+                    } else if (hours < 10){
+                        System.out.println("0" + hours + ":" + minutes + ":" + seconds);
+                    } else {
+                        System.out.println(hours + ":" + minutes + ":" + seconds);
+                    }
+                }
+            } else if (hours < 23){
+                if (hours < 10) {
+                    if (seconds < 10) {
+                        System.out.println("0" + hours + ":" + minutes + ":0" + seconds);
+                    } else {
+                        System.out.println("0" + hours + ":" + minutes + ":" + seconds);
+                    }
+                } else {
                     System.out.println(hours + ":" + minutes + ":" + seconds);
                 }
-            } else if (seconds == 59){
-                System.out.println(hours + ":" + (minutes + 1) + ":00");
             } else {
                 System.out.println(hours + ":" + minutes + ":" + seconds);
             }
         } else if (minutes < 59){
-            System.out.println(hours + ":" + (minutes + 1) + ":00");
+            minutes++;
+            if (minutes < 10) {
+                if (hours < 10) {
+                    System.out.println("0" + hours + ":0" + minutes + ":00");
+                }
+            } else if (hours < 10) {
+                System.out.println("0" + hours + ":" + minutes + ":00");
+            } else {
+                System.out.println(hours + ":" + minutes + ":00");
+            }
         } else if (hours < 23){
-            System.out.println((hours + 1) + ":00:00");
+            hours++;
+            if (hours < 10) {
+                System.out.println("0" + hours + ":00:00");
+            } else {
+                System.out.println(hours + ":00:00");
+            }
         } else {
             System.out.println("00:00:00");
         }
+
     }
 }
