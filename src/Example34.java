a18633d6fa2060bc8923585eb2f0484d66b5d159
joseluis-muriach <alumur497@ieselcaminas.org>
2022-10-06 12:13:33 +0200

    Example34 completed

---
+++ src/Example34.java
@@ -0,0 +1,11 @@
+public class Example34 {
+    public static void main(String[] args) {
+
+        int e = 1;
+
+        for (int i = 1; i <= 10; i++){
+            e = e * i;
+            System.out.println("e = " + i + " / " + e);
+        }
+    }
+}
